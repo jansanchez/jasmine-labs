4d3217c93c1ff9e958c0c685f1559144bb9fae39
Jan Sanchez <joejansanchez@gmail.com>
2014-03-06 14:13:51 -0500

    estructuring files

---
+++ Gruntfile.js
@@ -0,0 +1,98 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    //coffee
+    coffee: {
+      glob_to_multiple: {
+        options: {
+          bare: true
+        },
+        expand: true,
+        cwd: 'frontend/coffee/',
+        src: ['**/*.coffee','!**/_*.coffee'],
+        dest: 'public/js/',
+        ext: '.js'
+      }
+    },
+    //jshint
+    jshint: {
+      options: {
+        jshintrc : '.jshintrc'
+      },
+      js: ['public/js/src/*.js']
+    },
+    //watch
+    /*echo fs.inotify.max_user_watches=524288 | sudo tee -a /etc/sysctl.conf && sudo sysctl -p*/
+    watch: {
+      coffee: {
+        files: 'frontend/coffee/**/*.coffee',
+        tasks: ['coffee', 'jshint'],
+        options: {
+          interrupt: true
+        }
+      },
+      jade: {
+        files: ['frontend/jade/*.jade'],
+        tasks: ['jade'],
+        options: {
+          pretty: true,
+          interrupt: true
+        }
+      },
+      stylus: {
+        files: ['frontend/styl/*.styl'],
+        tasks: ['stylus'],
+        options: {
+          interrupt: true
+        }
+      },
+    },
+    //jade
+    jade: {
+      compile: {
+        options: {
+          pretty: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'frontend/jade/',
+          src: [ '**/*.jade', '!config/*.jade', '!layout/*.jade', '!render/*.jade'],
+          dest: 'html',
+          ext: '.html'
+        }]
+      }
+    },
+    //stylus
+    stylus: {
+      compile: {
+        options: {
+          paths: ['frontend/stylus']
+        },
+        files: [{
+          expand: true,
+          cwd: 'frontend/styl/',
+          src: [ '**/*.styl'],
+          dest: 'public/css',
+          ext: '.css'
+        }]
+      }
+    }
+  });
+
+  //loadNpmTasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jade');
+  grunt.loadNpmTasks('grunt-contrib-stylus');
+
+  // Run Default task(s).
+  grunt.registerTask('html', ['jade']);
+  grunt.registerTask('js', ['coffee', 'jshint']);
+  grunt.registerTask('css', ['stylus']);
+  grunt.registerTask('default', ['html', 'js', 'css']);
+
+
+
+};
---
+++ frontend/coffee/src/Player.coffee
@@ -0,0 +1,18 @@
+Player = () ->
+Player::play = (song) ->
+	@currentlyPlayingSong = song
+	@isPlaying = true
+	return
+
+Player::pause = ->
+	@isPlaying = false
+	return
+
+Player::resume = ->
+	throw new Error("song is already playing")  if @isPlaying
+	@isPlaying = true
+	return
+
+Player::makeFavorite = ->
+	@currentlyPlayingSong.persistFavoriteStatus true
+	return
